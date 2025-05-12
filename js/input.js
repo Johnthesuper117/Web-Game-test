c3214e2de14f5dadc3b9e9932bc74976c04092fd
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-05-12 10:08:39 -0400

    Create input.js

---
+++ js/input.js
@@ -0,0 +1,9 @@
+const keys = {};
+
+window.addEventListener('keydown', (event) => {
+    keys[event.key] = true;
+});
+
+window.addEventListener('keyup', (event) => {
+    keys[event.key] = false;
+});
